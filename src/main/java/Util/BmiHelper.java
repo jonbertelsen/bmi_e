a095a998cb59ceaca0718fd0f010cefe4b1d24ec
Jon Bertelsen <jobe@cphbusiness.dk>
2020-03-26 17:00:29 +0100

    Minor javadoc update

--- src/main/java/Util/BmiHelper.java
+++ src/main/java/Util/BmiHelper.java
@@ -1,11 +1,27 @@
 package Util;
 
+
+/**
+ * Hjælpefunktioner til BMI web
+ */
 public class BmiHelper {
 
+    /**
+     *
+     * @param height in cm
+     * @param weight in kg
+     * @return bmi calculated
+     * @author jon
+     */
     public static Double calcBMI(Double height, Double weight){
         return weight / (height * height) * 100 * 100;
     }
 
+    /**
+     *
+     * @param bmi value
+     * @return A bmi category text
+     */
     public static String getCategory(Double bmi){
         if (bmi > 30){
             return "Svært overvægtig";
